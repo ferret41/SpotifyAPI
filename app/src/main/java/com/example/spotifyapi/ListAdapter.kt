a4c9c396323d9d08f22c5b295d329f53fb12a329
Ferret41 <martiferret41@gmail.com>
2020-09-11 11:26:35 +0200

    Artists list added

--- app/src/main/java/com/example/spotifyapi/ListAdapter.kt
+++ app/src/main/java/com/example/spotifyapi/ListAdapter.kt
@@ -4,8 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.artist_card.view.*
 
 class ListAdapter: RecyclerView.Adapter<ArtistViewHolder>() {
+
+    var artists = listOf("Amaia", "Bob", "Mike")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtistViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -14,16 +17,16 @@ class ListAdapter: RecyclerView.Adapter<ArtistViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ArtistViewHolder, position: Int) {
-
+        holder.view.artistTextView.text = artists.get(position)
     }
 
     override fun getItemCount(): Int {
-        return 5
+        return artists.size
     }
 
 
 }
 
-class ArtistViewHolder(v: View): RecyclerView.ViewHolder(v) {
+class ArtistViewHolder(val view: View): RecyclerView.ViewHolder(view) {
 
 }
